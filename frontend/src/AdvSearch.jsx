a40ffa8ebdf68483c22c32dd8a2e9ee520e69647
Ryan Schmitt <ryan15schmitt@gmail.com>
2024-04-13 14:29:51 -0700

    Created NavBar

---
+++ frontend/src/AdvSearch.jsx
@@ -0,0 +1,14 @@
+function AdvSearch() {
+    return (
+      <div style={{ margin: "30px"}}>
+        <input
+          type="text"
+          className="w-75 form-control position-absolute bottom-0"
+          style={{ margin: "40px 150px"}}
+        />
+      </div>
+    );
+  }
+  
+  export default AdvSearch;
+  
