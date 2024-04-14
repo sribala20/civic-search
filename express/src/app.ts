215dac0ca009b77deed44da4439725d3515b9ddf
gusflus <gus.flusser@gmail.com>
2024-04-14 01:03:59 -0700

    express and better landing

---
+++ express/src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const PORT = 8080;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
