3e89281423885717b2fc68d32dfcd6e88c81db82
gusflus <gus.flusser@gmail.com>
2024-04-14 11:13:27 -0700

    upload pdf

--- website/src/pages/Upload.tsx
+++ website/src/pages/Upload.tsx
@@ -1,11 +1,50 @@
+import { useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "../styles.css";
 
 const UploadPage = () => {
-  const handleFileUpload = (e: any) => {
-    // Handle file upload logic here
-    console.log("File uploaded:", e.target.files[0]);
+  const [files, setFiles] = useState<FileList | null>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFiles(e.target.files);
+    }
+  };
+
+  const handleFileUpload = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log("Uploading files...");
+
+    if (!files) {
+      console.error("No files selected!");
+      return;
+    }
+
+    e.preventDefault();
+
+    const formData = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      formData.append("file", files[i]);
+    }
+
+    const options = {
+      method: "POST",
+      "Content-Type": "multipart/form-data",
+      body: formData,
+    };
+
+    console.log("fetching... ");
+    fetch("http://localhost:8080/api/v1/upload/document/", options)
+      .then((data) => {
+        console.log("Files uploaded successfully!");
+        console.log(data);
+        setFiles(null);
+      })
+      .catch((error) => {
+        console.error("Error uploading files:", error);
+      });
+
+    setFiles(null);
   };
 
   return (
@@ -15,15 +54,10 @@ const UploadPage = () => {
         <div className="upload-page">
           <h1>Upload Files</h1>
           <div className="form-container">
-            <form>
+            <form onSubmit={handleFileUpload}>
               <div className="input-group">
                 <label htmlFor="fileInput">Select file(s) to upload:</label>
-                <input
-                  type="file"
-                  id="fileInput"
-                  multiple
-                  onChange={handleFileUpload}
-                />
+                <input type="file" id="fileInput" onChange={handleFileChange} />
               </div>
               <button type="submit">Upload</button>
             </form>
