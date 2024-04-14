f33848f1b34941879626b25496fd993a4e2b94ce
calvinvanoss <cvanoss@calpoly.edu>
2024-04-14 10:02:53 -0700

    retrieve endpoint

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,10 +1,7 @@
 import cors from 'cors'
 import express from 'express'
 import bodyParser from 'body-parser'
-
-//import Database from '...'
-
-//import { applyKeywordItemSearch } from '...'
+import { BedrockAgentRuntimeClient, RetrieveCommand } from "@aws-sdk/client-bedrock-agent-runtime";
 
 const app = express()
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -13,25 +10,30 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 const port = 8000
 
+const client = new BedrockAgentRuntimeClient({ region: 'us-east-1' })
+
 app.get('/', (req, res) => {
   res.status(200).send('Hello, world!')
 })
 
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
-
-// Define the route to handle user data
-app.post('/search', (req, res) => {
-  // Extract data from the request body
-  const { search } = req.body;
-
-  // Perform any necessary backend processing with the received data
-  const response_data = {
-    message: 'Data received successfully!',
-    query: search
+app.post('/retrieve', async (req, res) => {
+  const knowledgeBaseId = 'U03XIQYOWK'
+  const input = {
+    knowledgeBaseId: knowledgeBaseId,
+    retrievalQuery: {
+      text: req.body.text,
+    },
+    // Add other parameters as needed
   };
-  // Return the response as JSON
-  res.json(response_data);
+
+  const command = new RetrieveCommand(input);
+
+  try {
+    const response = await client.send(command);
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
 });
 
 app.listen(process.env.PORT || port, () => {
