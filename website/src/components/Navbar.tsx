43eeaa3bca9c09a8d15d9ab0f2f9115477625462
gusflus <gus.flusser@gmail.com>
2024-04-14 00:23:56 -0700

    navbar and css updates

---
+++ website/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import logo from "../assets/logo.png";
+import "../styles.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo-container">
+        <img src={logo} alt="City of San Luis Obispo" className="logo" />
+      </div>
+      <ul className="nav-links">
+        <li>
+          <a href="/search" className="active">
+            Search
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- website/src/pages/Search.tsx
+++ website/src/pages/Search.tsx
@@ -1,7 +1,49 @@
+import Navbar from "../components/Navbar";
+import "../styles.css";
+
 const Search = () => {
   return (
     <div>
-      <h1>Search</h1>
+      <Navbar />
+      <div className="search-page">
+        <h1>Request for Public Records</h1>
+        <div className="form-container">
+          <form>
+            <div className="input-group">
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" placeholder="Last, First" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="address">Address:</label>
+              <input type="text" id="address" placeholder="Street & Unit #" />
+              <input type="text" id="city" placeholder="City" />
+              <input type="text" id="state" placeholder="State" />
+              <input type="text" id="zip" placeholder="Zip" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" placeholder="Email" />
+              <label htmlFor="phone">Phone:</label>
+              <input type="tel" id="phone" placeholder="Phone" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="type">Type:</label>
+              <input type="text" id="type" placeholder="Type" />
+              <label htmlFor="incidentNumber">Incident Number:</label>
+              <input
+                type="text"
+                id="incidentNumber"
+                placeholder="Incident Number"
+              />
+            </div>
+            <div className="record-info">
+              <h2>Record Information:</h2>
+              <textarea placeholder="List the records you are requesting. Specify relevant information such as: subject, title, location, address, person(s) involved, project name, etc."></textarea>
+            </div>
+            <button type="submit">Submit Request</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
